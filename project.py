accbb05549cfd3a9fd32b12d15362e197550924b
corinnpope <corinn.pope@gmail.com>
2017-04-10 16:21:31 -0500

    port interfering with heroku deployment

--- project.py
+++ project.py
@@ -501,5 +501,6 @@ def deleteTactic(strategy_id, tactic_id):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)    
+    # was app.run(host='0.0.0.0', port=5000)  
+    app.run()    
 
